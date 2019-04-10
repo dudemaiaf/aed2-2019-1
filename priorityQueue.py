d1597eea5244f4f1e774d1da4289a193f9b2cd3d
Eduardo Maia Freire <dudu_maia13@hotmail.com>
2019-04-10 16:12:52 -0400

    Add files via upload

--- priorityQueue.py
+++ priorityQueue.py
@@ -54,6 +54,32 @@ print(inserir(heap, 35, c))
 
 #print(heap)
 
+#------------------------------------------------------------------
+
+class FilaDePrioridade:
+
+	def __init__(self):
+		self._fila = []
+		self._indice = 0
+
+	def inserir(self, item, prioridade):
+		heapq.heappush(self._fila, (-prioridade, self._indice, item))
+		self._indice += 1
+
+	def remover(self):
+		return heapq.heappop(self._fila)[-1]
 
 
-    
+class Pessoa:
+	def __init__(self, nome):
+		self.nome = nome
+
+	def __repr__(self):
+		return self.nome
+
+
+fila = FilaDePrioridade()
+fila.inserir(Pessoa('Maria'), 20)
+fila.inserir(Pessoa('Pedro'), 16)
+fila.inserir(Pessoa('Felipe'), 25)
+fila.inserir(Pessoa('Carol'), 23)
